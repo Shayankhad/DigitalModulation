4369f497f8c755830538758e53a71d21be4a5402
Shayankhad <168594183+Shayankhad@users.noreply.github.com>
2025-12-22 17:55:49 +0330

    updated dds dp

--- DDS_DataPath.v
+++ DDS_DataPath.v
@@ -1,5 +1,5 @@
 module DDS_Data_Path (
-    input clk , input rst , reg output [8:0] out
+    input clk , input rst , output [8:0] out
 );
     wire [5:0] rom_in;
     wire [7:0] rom_out;
@@ -18,12 +18,17 @@ module DDS_Data_Path (
     );
 
     Phase_Acc ACC(rst,clk, bit_count , sign , phase);
-    mux2to1 Rom_Addr(6)(bit_count,bit_count_tows,phase,rom_in);
-    mux2to1 Rom_Out(8)(rom_out,8'd255,Rom_out_sel,rom_out_selected);
+    mux2to1 Rom_Addr #(6)(bit_count,bit_count_tows,phase,rom_in);
+    mux2to1 Rom_Out #(8)(rom_out,8'd255,Rom_out_sel,rom_out_selected);
     Twos_comp Twos_1(bit_count,bit_count_tows);
     nor_reduction_6bit Nor(bit_count,bit_count_nor);
     And_gate and_gate_mod (bit_count_nor,phase,Rom_out_sel);
-    Twos_comp Twos_2(rom_out_selected,out);
+
+    Second_twos_comp scond_t_comp(
+        .sign_bit(sign),
+        .inp(rom_out_selected),
+        .out(out)
+    );
 
 
 endmodule
