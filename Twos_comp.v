5e2ad7ea367695d248bf8766d3004e0bd86c9c4c
Shayankhad <168594183+Shayankhad@users.noreply.github.com>
2025-12-22 17:02:37 +0330

    updated Twos_comp

--- Twos_comp.v
+++ Twos_comp.v
@@ -1,7 +1,8 @@
-module Twos_comp (
-    input  wire [5:0] data_in, 
-    output wire [5:0] data_out  
+module Twos_comp #(parameter WIDTH = 6) (
+    input  wire [WIDTH-1:0] data_in, 
+    output wire [WIDTH-1:0] data_out  
 );
+
     assign data_out = (~data_in) + 1'b1;
 
 endmodule
