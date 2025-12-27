87f719aa1d0945000d3a913e072b83dc1e191ec2
AlirezaAbedizadeh <168010030+AlirezaAbedizadeh@users.noreply.github.com>
2025-12-27 23:38:36 +0330

    fix compile error

--- DDS_DataPath.v
+++ DDS_DataPath.v
@@ -18,8 +18,8 @@ module DDS_Data_Path (
     );
 
     Phase_Acc ACC(rst,clk, bit_count , sign , phase);
-    mux2to1 Rom_Addr #(6)(bit_count,bit_count_tows,phase,rom_in);
-    mux2to1 Rom_Out #(8)(rom_out,8'd255,Rom_out_sel,rom_out_selected);
+    mux2to1 #(6) Rom_Addr (bit_count,bit_count_tows,phase,rom_in);
+    mux2to1 #(8) Rom_Out (rom_out,8'd255,Rom_out_sel,rom_out_selected);
     Twos_comp Twos_1(bit_count,bit_count_tows);
     nor_reduction_6bit Nor(bit_count,bit_count_nor);
     And_gate and_gate_mod (bit_count_nor,phase,Rom_out_sel);
